e47cb89bd36d9d40e670040b1a28fed18141ac1a
michelca1pi-web <michelca1pi@gmail.com>
2025-09-08 11:28:55 +0000

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~RmFT2JxB#4xunSQGLJohJIlhkVp-tMcJGR50UVY2rgHoXIY90bzs",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "+2250500694652", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
